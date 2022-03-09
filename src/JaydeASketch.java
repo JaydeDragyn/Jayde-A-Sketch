2773b7eaae5df0b3382459fc3ddc9c3cdde32921
Jayde Dragyn <jaydedragyn@outlook.com>
2022-03-08 23:33:55 -0500

    Display in Frame

--- src/JaydeASketch.java
+++ src/JaydeASketch.java
@@ -1,11 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JaydeASketch extends JPanel {
+
+    private JFrame frame;
 
     public JaydeASketch() { }
 
     public void initialize() {
+        setPreferredSize(new Dimension(800, 600));
+        setBackground(Color.BLACK);
 
+        frame = new JFrame("Jayde-A-Sketch");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(this);
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) { SwingUtilities.invokeLater(JaydeASketch::createAndShowDisplay);}
