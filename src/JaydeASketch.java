d91cf8c4d96536c05009dd10b34c51293180d388
Jayde Dragyn <jaydedragyn@outlook.com>
2022-03-08 23:57:14 -0500

    Add initDrawingSurface() and add .paintComponent()

--- src/JaydeASketch.java
+++ src/JaydeASketch.java
@@ -1,19 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
 
 public class JaydeASketch extends JPanel {
 
     private JFrame frame;
     private final Dimension PANEL_SIZE;
 
+    private final Color SURFACE_DEFAULT;
+    private BufferedImage drawingSurface;
+    private Graphics drawingSurfacePen;
+    private Point penLocation;
+
     public JaydeASketch() {
         PANEL_SIZE = new Dimension(800,600);
+        SURFACE_DEFAULT = new Color(168,168,168);
     }
 
     public void initialize() {
         initPanel();
         initFrame();
-
+        initDrawingSurface();
     }
 
     private void initPanel() {
@@ -28,6 +35,21 @@ public class JaydeASketch extends JPanel {
         frame.add(this);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void initDrawingSurface() {
+        drawingSurface = new BufferedImage(PANEL_SIZE.width, PANEL_SIZE.height, BufferedImage.TYPE_INT_RGB);
+        drawingSurfacePen = drawingSurface.getGraphics();
+        drawingSurfacePen.setColor(SURFACE_DEFAULT);
+        drawingSurfacePen.fillRect(0,0, PANEL_SIZE.width, PANEL_SIZE.height);
+        penLocation = new Point(PANEL_SIZE.width / 2, PANEL_SIZE.height / 2);
+
+        paintComponent(getGraphics());
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        g.drawImage(drawingSurface, 0, 0, null);
     }
 
     public static void main(String[] args) { SwingUtilities.invokeLater(JaydeASketch::createAndShowDisplay);}
